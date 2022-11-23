c0aeb23800d0df514b95e24756ffccf035bcfedf
HammadAli55 <hammadalii@outlook.com>
2022-11-23 16:18:31 +0500

    reduce operator

--- Operators/reduce-operator.js
+++ Operators/reduce-operator.js
@@ -5,8 +5,14 @@ import { reduce } from 'rxjs/operators'
   Applies an accumulator function over the source Observable, and 
   returns the accumulated result when the source completes, given an 
   optional seed value.
-  '0' that is called seed value that is optional and represents starting value. Starting value should start at 0  
-  new numbers are added in seed value
+  
+  Seed value '0': 
+  It is optional and represents starting value. 
+  Starting value should start at 0 where new numbers are added in seed value.
+
+  Accumulator (acc): 
+  Stores value previously return by this function. 
+  Value (val): It references the value emitted by the observable
 */
 
 const observable = of(1,2,3,4,5).pipe(
